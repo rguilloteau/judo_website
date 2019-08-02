8cfdb6a5616335fe9f06f77b394bbc966b24dda6
Romain Guilloteau <romain.guilloteau@telecom-paristech.fr>
2019-08-02 11:54:55 +0200

    Add Event model and Department model

--- agenda/models.py
+++ agenda/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class event(models.Model):
+    title = models.CharField(max_length=100)
+    organizer = models.CharField(max_length=42)
+    date_beginning = models.DateTimeField()
+    date_end = models.DateTimeField(null=True)
+    department = models.ForeignKey('Department', on_delete=models.SET_NULL, null=True)
+    location = models.CharField(max_length=100)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['date_beginning']
+        
+    def __str__(self):
+        return self.title
+
+class Department(models.Model):
+    name = models.CharField(max_length=30)
+    number = models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- judo/settings.py
+++ judo/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'fr-FR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
