0d35df21f3198fbb8d4841a2396e446b6e238294
Romain Guilloteau <romain.guilloteau@telecom-paristech.fr>
2019-08-05 13:09:13 +0200

    Contact form now send email

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
 from .forms import ContactForm
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def send_mail(dest, mail_sender, sender_passwd, subject, message):
+
+    msg = MIMEMultipart()
+    msg['From'] = mail_sender
+    msg['To'] = dest
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(message))
+
+    mailserver = smtplib.SMTP('smtp.gmail.com', 587)
+    mailserver.ehlo()
+    mailserver.starttls()
+    mailserver.ehlo()
+    mailserver.login(mail_sender, sender_passwd)
+    mailserver.sendmail(mail_sender, dest, msg.as_string())
+    mailserver.quit()
 
 def contact(request):
     
@@ -7,11 +27,15 @@ def contact(request):
     
     if form.is_valid(): 
         # Ici nous pouvons traiter les données du formulaire
-        sujet = form.cleaned_data['subject']
+        subject = form.cleaned_data['subject']
         message = form.cleaned_data['message']
-        envoyeur = form.cleaned_data['sender']
-        renvoi = form.cleaned_data['send_back']
+        sender = form.cleaned_data['sender']
+        send_back = form.cleaned_data['send_back']
 
+        if send_back:
+            send_mail(sender, "normandie.judo@gmail.com", '=S/]/Q"6HHt6', subject, message)
+        
+        send_mail("romain.guilloteau@outlook.com", "normandie.judo@gmail.com", '=S/]/Q"6HHt6', subject, message)
         envoi = True
     
     # Quoiqu'il arrive, on affiche la page du formulaire.
--- judo/urls.py
+++ judo/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('contact/',include('contact.urls')),
-    path('agenda/', include('agenda.urls')),
+    path('', include('agenda.urls')),
 ]
